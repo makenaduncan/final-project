7833a6f4269d40bfe9df2280d98a1a4c04fec787
makena <mduncan18@byui.edu>
2021-12-04 21:44:10 -0700

    attempted to fix errors

--- batter/__main__.py
+++ batter/__main__.py
@@ -11,7 +11,6 @@ from game.audio_service import AudioService
 from game.detective import Detective
 from game.weapon import Weapon
 from game.people import People
-from game.gameGenerator import GameGenerator
 from game.moveActorsAction import MoveActorsAction
 from game.controlActorsAction import ControlActorsAction
 # from game.handle_collisions_action import HandleCollisionsAction
@@ -22,11 +21,60 @@ def main():
     # create the cast {key: tag, value: list}
     cast = {}
 
-    gameGenerator = GameGenerator()
-    cast["boxes"]=gameGenerator.get_detective(cast)
-    cast["weapons"]= gameGenerator.get_weapons(cast)
-    cast["people"]=gameGenerator.get_people(cast)
-    print(GameGenerator)
+    boxes = []
+    box = Detective()
+    boxes.append(box)
+    cast["boxes"] = boxes
+
+    weapons = []
+    knife = Weapon(constants.IMAGE_WEAPON)
+    rope = Weapon(constants.IMAGE_WEAPON)
+    pistol = Weapon(constants.IMAGE_WEAPON)
+    candlestick = Weapon(constants.IMAGE_WEAPON)  
+    poison = Weapon(constants.IMAGE_WEAPON)
+    trophy = Weapon(constants.IMAGE_WEAPON)
+    bat = Weapon(constants.IMAGE_WEAPON)
+    dumbbell = Weapon(constants.IMAGE_WEAPON)
+    weapons.append(knife)
+    weapons.append(rope)
+    weapons.append(pistol)
+    weapons.append(candlestick)
+    weapons.append(poison)
+    weapons.append(trophy)
+    weapons.append(bat)
+    weapons.append(dumbbell)
+    cast["weapons"] = weapons
+    # for weapon in weapons:
+    #     weapon.set_text(constants.W_CLUES)
+    #     weapon.set_status(False)
+    #put the code to randomly select a weapon
+    chosen_index = random.randint(0,len(weapons))
+    chosen_weapon = weapons[chosen_index]
+    chosen_weapon.set_text("this is the murder weapon")
+    chosen_weapon.set_status(True)
+
+    people = []
+    mustard = People(constants.IMAGE_PERSON)
+    plum = People(constants.IMAGE_PERSON)
+    green = People(constants.IMAGE_PERSON)
+    peacock = People(constants.IMAGE_PERSON)
+    scarlet = People(constants.IMAGE_PERSON)
+    white = People(constants.IMAGE_PERSON)
+    people.append(mustard)
+    people.append(plum)
+    people.append(green)
+    people.append(peacock)
+    people.append(scarlet)
+    people.append(white)
+    cast["people"] = people
+    # for person in people:
+    #     person.set_text(constants.P_CLUES)
+    #     person.set_status(False)
+    #put the code to randomly select a weapon
+    # chosen_index = random.randint(0,len(people))
+    # chosen_person = people[chosen_index]
+    # chosen_person.set_text("This is the murderer")
+    # chosen_person.set_status(True)
 
     # Finish creating the cast 
 
